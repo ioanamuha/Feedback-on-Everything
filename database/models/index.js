c3e849d679a35c411d974ea7b2e16d59f644f4db
Ioana Muha <125951963+ioanamuha@users.noreply.github.com>
2024-06-25 07:54:32 +0300

    Delete database directory

--- database/models/index.js
+++
@@ -1,9 +0,0 @@
-const UserModel = require("./user.model");
-const FormModel = require("./form.model");
-const UserFormsModel = require("./user-form.model");
-
-module.exports = {
-	UserModel,
-	FormModel,
-	UserFormsModel,
-};
