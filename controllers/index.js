2d8842041285ca536ae487219b7ded35eefc62f5
Corina2003 <126088447+Corina2003@users.noreply.github.com>
2024-06-25 07:51:25 +0300

    update be

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const AuthRouter = require("./auth.controller");
+const UserRouter = require("./user.controller");
+const FormRouter = require("./form.controller");
+
+module.exports = {
+	AuthRouter,
+	UserRouter,
+	FormRouter,
+};
